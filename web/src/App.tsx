3512fa716fbe5b2a296a6bda5d12cc52f1a6a007
Marcos Porto <mosther_marcos007@hotmail.com>
2022-10-20 17:06:03 -0300

    Primeiro git

---
+++ web/src/App.tsx
@@ -0,0 +1,68 @@
+import './styles/main.css';
+import logo from './assets/logo-nlw-esports.svg'
+import { GameBanner } from './components/GameBanner';
+import { CreatedAdBanner } from './components/CreateAdBanner';
+import { useEffect, useState } from 'react';
+import * as Dialog from '@radix-ui/react-dialog'
+import { GameController } from 'phosphor-react';
+import { Input } from './components/Form/input';
+import { CreateAdModal } from './components/CreateAdModal';
+import axios from 'axios';
+ 
+interface Game{
+    id: string;
+    title: string;
+    bannerUrl: string;
+    _count: {
+      ads: number;
+    }
+}
+
+function App() {
+    const [games, setGames] = useState<Game[]>([]);
+
+    // useEffect(() => {
+    //   fetch('http://localhost:3333/games')
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     console.log(data)
+    //     setGames(data)
+    //   })
+    // }, [])
+
+    useEffect(() => {
+      axios('http://localhost:3333/games') .then(response => {
+        setGames(response.data)
+      })
+    }, [])
+    
+return (
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+        <img src={logo} alt=""/>
+
+        <h1 className="text-6xl text-white font-black mt-20">Seu <span className="text-transparent bg-nlw-gradient bg-clip-text"> duo </span> está aqui.</h1>
+        <div className=" grid grid-cols-6 gap-6 mt-16">
+          {games.map(game => {
+            return (
+              <GameBanner 
+              key={game.id}
+              bannerUrl={game.bannerUrl} 
+              title={game.title}
+              adsCount={game._count.ads}
+              />
+           )
+          })}
+  </div>
+        
+      <Dialog.Root>
+          <CreatedAdBanner/>
+
+         <CreateAdModal/>
+      </Dialog.Root>
+       
+    </div>
+    
+  )
+}
+
+export default App
